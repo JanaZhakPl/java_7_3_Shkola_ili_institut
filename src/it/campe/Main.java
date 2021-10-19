e1199a0e72b0e829f57d71ceec75de856e7fecf4
Ksenx73 <hello@itcampe.com>
2021-10-19 11:43:14 +0300

    first commit

---
+++ src/it/campe/Main.java
@@ -0,0 +1,14 @@
+package it.campe;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
+        if (age < 18)
+            if (age >= 6)
+                System.out.println("нужно ходить в школу");
+            else
+                System.out.println("пора в институт");
+    }
+}
